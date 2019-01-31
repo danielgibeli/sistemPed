6a97dc0f34720cef66d43f852041e6117cb75c99
Daniel Gibeli <danielgibeli@gmail.com>
2019-01-31 17:31:39 -0200

    First commit

---
+++ SistemPed/Entities/Order.cs
@@ -0,0 +1,35 @@
+﻿using SistemPed.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemPed.Entities
+{
+    class Order
+    {
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public List<Client> Clients { get; set; } = new List<Client>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime date, OrderStatus status)
+        {
+            Date = date;
+            Status = status;
+        }
+
+        public void AddItem(OrderItem orderItem)
+        {
+            Items.Add(orderItem);
+        }
+        public void RemoveItem(OrderItem orderItem)
+        {
+            Items.Remove(orderItem);
+        }
+
+    }
+}
---
+++ SistemPed/Entities/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemPed.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
